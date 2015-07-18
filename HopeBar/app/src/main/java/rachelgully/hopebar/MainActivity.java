117efcd73d36f29a19e5d18bd3799e93a304617f
Mootix1313 <rachellehcar.gully1@gmail.com>
2015-07-18 06:26:36 -0700

    added stuff

--- HopeBar/app/src/main/java/rachelgully/hopebar/MainActivity.java
+++ HopeBar/app/src/main/java/rachelgully/hopebar/MainActivity.java
@@ -3,71 +3,36 @@ package rachelgully.hopebar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-    private ImageButton about;
-    private ImageButton thebar;
-    private ImageButton impact;
-    private ImageButton buy;
-    private ImageButton share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
 
-        /*open about*/
-        about = (ImageButton) findViewById(R.id.about);
-        about.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, About.class);
-                startActivity(i);
-            }
-        });
+        /*splash screen setup*/
+        Thread logoTimer = new Thread() {
+            public void run(){
+                try{
+                    int logoTimer = 0;
+                    while(logoTimer < 5000){
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    };
+                    startActivity(new Intent("com.tutorial.CLEARSCREEN"));
+                }
 
-        /*open thebar*/
-        thebar = (ImageButton) findViewById(R.id.thebar);
-        thebar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, TheBar.class);
-                startActivity(i);
-            }
-        });
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-        /*open impact*/
-        impact = (ImageButton) findViewById(R.id.impact);
-        impact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Impact.class);
-                startActivity(i);
+                finally{
+                    finish();
+                }
             }
-        });
+        };
 
-        /*open buy*/
-        buy = (ImageButton) findViewById(R.id.buy);
-        buy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Buy.class);
-                startActivity(i);
-            }
-        });
-
-        /*open share*/
-        share = (ImageButton) findViewById(R.id.share);
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Share.class);
-                startActivity(i);
-            }
-        });
+        logoTimer.start();
     }
 }
---
+++ HopeBar/app/src/main/java/rachelgully/hopebar/MainScreen.java
@@ -0,0 +1,74 @@
+package rachelgully.hopebar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainScreen extends Activity {
+
+    private ImageButton about;
+    private ImageButton thebar;
+    private ImageButton impact;
+    private ImageButton buy;
+    private ImageButton share;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*open about*/
+        about = (ImageButton) findViewById(R.id.about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, About.class);
+                startActivity(i);
+            }
+        });
+
+        /*open thebar*/
+        thebar = (ImageButton) findViewById(R.id.thebar);
+        thebar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, TheBar.class);
+                startActivity(i);
+            }
+        });
+
+        /*open impact*/
+        impact = (ImageButton) findViewById(R.id.impact);
+        impact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, Impact.class);
+                startActivity(i);
+            }
+        });
+
+        /*open buy*/
+        buy = (ImageButton) findViewById(R.id.buy);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, Buy.class);
+                startActivity(i);
+            }
+        });
+
+        /*open share*/
+        share = (ImageButton) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, Share.class);
+                startActivity(i);
+            }
+        });
+    }
+}
